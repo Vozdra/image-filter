32e1f00994feccbab6a08ee901f8bc5f764f71d2
Vozdra <vozdr@ukr.net>
2019-02-28 19:29:58 +0200

    cleaning upload directory

--- clear.php
+++ clear.php
@@ -3,7 +3,7 @@
 if (!file_exists("img-uploads")) return;
 
 $dir = "img-uploads";
-$files = new FilesystemIterator($dir)
+$files = new FilesystemIterator($dir);
 
 foreach ($files as $file) {      
   if (file_exists($file)) {
